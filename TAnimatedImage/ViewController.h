a6aebeb2bf44e628748cedec852c0fb3d53152dd
LQblue <1521224526@qq.com>
2018-03-14 16:54:36 +0800

    Initial Commit

---
+++ TAnimatedImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TAnimatedImage
+//
+//  Created by liqi on 2018/3/14.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
